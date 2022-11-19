e8f8d25bff00925fdca417a160a79321daf81ec1
Samm <60794497+Sammyueru@users.noreply.github.com>
2022-11-19 12:33:54 -0700

    Update scratchworks.js

--- scratchworks.js
+++ scratchworks.js
@@ -1,4 +1,5 @@
-window.document.head.innerHTML = window.document.head.innerHTML + '<script type="module" src="https://sammyueru.github.io/Scratchworks/require.js"></script>';
+//window.document.head.innerHTML = window.document.head.innerHTML + '<script type="module" src="https://sammyueru.github.io/Scratchworks/require.js"></script>';
+document.getElementsByTagName('head')[0].appendChild('<script type="module" src="https://sammyueru.github.io/Scratchworks/require.js"></script>');
 import { require } from "https://sammyueru.github.io/Scratchworks/require.js";
 //import { greenworks } from "https://sammyueru.github.io/Scratchworks/greenworks.js";
 
