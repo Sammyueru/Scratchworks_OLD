f23679da269b7d35da3fb291924b8b7e5880cfb7
Samm <60794497+Sammyueru@users.noreply.github.com>
2022-11-19 13:12:29 -0700

    Update scratchworks.js

--- scratchworks.js
+++ scratchworks.js
@@ -7,13 +7,7 @@ window.document.head.innerHTML = window.document.head.innerHTML + '<script data-
 //window.webPreferences.nodeIntegration = true;
 
 require.config({
-    //By default load any module IDs from js/lib
     baseUrl: 'https://sammyueru.github.io/Scratchworks',
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
 });
 
 (function(Scratch) {
